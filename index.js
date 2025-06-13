a4aaf0e949d3afc9a6bc5e7b3248f230089a075a
Muchiri-cmd <davismuchiri21@gmail.com>
2025-06-13 16:39:46 +0300

    Add posts endpoints

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get("/users/:id", async (req, res) => {
         id,
       },
     });
-    res.status(200).json({ user });
+    user ? res.status(200).json( user ) : res.status(404).json({message:"User Not found"});
   } catch (error) {
     console.log(error);
     res.status(500).json({
@@ -59,6 +59,86 @@ app.post("/users", async (req, res) => {
       .json({ message: "Error creating new user, please try again later" });
   }
 });
+
+// Posts endpoints
+//Get all posts
+app.get("/posts", async(_req,res) => {
+    try{
+        const posts = await client.post.findMany({
+            where: {
+                isDeleted: false,
+            }
+        })
+        res.status(200).json(posts)
+    } catch(error){
+        console.log(error);
+        res.status(500).json({message:"Something went wrong, please try again later."})
+    }
+})
+
+//Get 1 post
+app.get("/posts/:id", async(req,res) => {
+    try{
+        console.log(req.params)
+        const {id} = req.params
+
+        const post = await client.post.findFirst({
+            where:{
+                id
+            }
+        })
+        post ? res.status(200).json(post) : res.status(404).json({message:"Post unavailable"})
+    } catch(error){
+        console.log(error);
+        res.status(500).json({message:"Something went wrong, please try again later."})
+    }
+})
+
+//Create a post
+app.post("/posts",async(req,res) => {
+    try{
+        console.log(req.body);
+        const { title,content,authorId } = req.body
+        const newPost = await client.post.create({
+            data:{
+                title,content,
+                author:{
+                    connect:{id:authorId},
+                }
+            }
+        })
+        res.status(200).json("Post created successfully")
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message:"Something went wrong, please try again later."})
+    }
+})
+
+//Update a post
+app.put("/posts/:id",async(req,res) => {
+    try{
+        console.log(req.body);
+        const { title, content } = req.body
+        const {id} = req.params
+
+        const updatedPost = await client.post.update({
+            where : {
+                id
+            },
+            data:{
+                title,content
+            }
+        })
+        res.status(200).json(updatedPost)
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message:"Something went wrong, please try again later."})
+    }
+})
+
+//Delete a post
+app.delete("/posts/:id",async(req,res) =>)
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
